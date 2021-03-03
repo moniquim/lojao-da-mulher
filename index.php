a3556971ff264f3f71d8e1c7c2348641562caf3f
Monica Lima <monica_cristinalima@hotmail.com>
2021-03-03 15:59:06 -0300

    trabalhando com cores no html

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
+
 <!DOCTYPE html>
 <html>
-<head>
-<title>Lojao da mulher</title>
-</head>
-<body>
-
-<h1>Lojao da mulher</h1>
-<p>Loja itinerante de bijuterias e maquiagens.</p>
-
-</body>
+   <head>
+      <title>Lojao da mulher</title>
+   </head>
+   <body>
+      <h1>Lojao da mulher</h1>
+      <p>Loja itinerante de bijuterias e maquiagens.</p>
+      <div style="width:100%;display:flex;height: 80px">
+         <div style="width:50%;background:blue"> </div>
+         <div style="width:50%;background:red"> </div>
+      </div>
+   </body>
 </html>
